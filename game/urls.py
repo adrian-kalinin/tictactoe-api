308f9124a77579779b4b930758c6e580011f58d5
Adrian Kalinin <adriankalinin@protonmail.com>
2023-02-09 23:34:37 +0200

    Refactor urls

--- game/urls.py
+++ game/urls.py
@@ -1,11 +1,10 @@
-from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from game import views
 
-router = DefaultRouter()
-router.register("games", views.GameViewSet)
-
 app_name = "game"
 
-urlpatterns = [path("", include(router.urls))]
+router = DefaultRouter()
+router.register("games", views.GameViewSet, basename="game")
+
+urlpatterns = router.urls
