b97d187be91ab7c8780fa253ad5cfc231482d029
Adrian Kalinin <adriankalinin@protonmail.com>
2023-02-10 20:57:55 +0200

    Add possible responses to documentation

--- game/views.py
+++ game/views.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
@@ -7,11 +7,55 @@ from game.serializers import GameLocationSerializer, GameSerializer
 
 
 @extend_schema_view(
-    list=extend_schema(description="Get all games."),
-    create=extend_schema(description="Start a new game."),
-    retrieve=extend_schema(description="Get a game."),
+    list=extend_schema(
+        description="Get all games.",
+        responses={
+            200: OpenApiResponse(
+                response=GameSerializer,
+                description=(
+                    "Successful response, returns an array of games, "
+                    "returns an empty array if no users found"
+                ),
+            ),
+            400: OpenApiResponse(description="Bad request"),
+            404: OpenApiResponse(description="Resource not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    ),
+    create=extend_schema(
+        description="Start a new game.",
+        responses={
+            201: OpenApiResponse(
+                response=GameLocationSerializer,
+                description="Game successfully started",
+            ),
+            400: OpenApiResponse(description="Bad request"),
+            404: OpenApiResponse(description="Resource not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    ),
+    retrieve=extend_schema(
+        description="Get a game.",
+        responses={
+            200: OpenApiResponse(
+                response=GameSerializer,
+                description="Successful response, returns the game",
+            ),
+            400: OpenApiResponse(description="Bad request"),
+            404: OpenApiResponse(description="Resource not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    ),
     # update=extend_schema(description="Post a new move to a game."),
-    destroy=extend_schema(description="Delete a game."),
+    destroy=extend_schema(
+        description="Delete a game.",
+        responses={
+            200: OpenApiResponse(description="Game successfully deleted"),
+            400: OpenApiResponse(description="Bad request"),
+            404: OpenApiResponse(description="Resource not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    ),
 )
 class GameViewSet(viewsets.ModelViewSet):
     """API for games"""
@@ -26,7 +70,6 @@ class GameViewSet(viewsets.ModelViewSet):
         "options",
     ]
 
-    @extend_schema(responses=GameLocationSerializer)
     def create(self, request, *args, **kwargs):
         """Return location link if game is successfully created"""
         response = super().create(request, *args, **kwargs)
