d29b446390aef5f5378c70792b0846316d1034ac
Adrian Kalinin <adriankalinin@protonmail.com>
2023-02-12 21:20:15 +0200

    Implement game flow for POST and PUT methods

--- game/serializers.py
+++ game/serializers.py
@@ -1,30 +1,98 @@
-from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+import random
 
-from game.models import Game
-from game.services import GameService
+from rest_framework import serializers
+
+from game.models import Game, GamePlayer, GameStatus
 
 
 class GameSerializer(serializers.ModelSerializer):
     """A game object"""
 
-    def validate_board(self, value):
+    def validate_board(self, board):
         """Check that board is valid"""
 
-        # Validate board field contents
-        if not GameService.is_board_valid(value):
-            raise ValidationError(
+        if len(board) != 9 or not all(c in "XO-" for c in board):
+            raise serializers.ValidationError(
                 "Ensure this field has 9 characters and contains only '-', 'X', or 'O'."
             )
 
-        # Validate first move on the board if it's a new game
-        if not self.instance:
-            if not GameService.is_first_move_valid(value):
-                raise ValidationError(
-                    "Ensure the board is either empty or contains only one 'X' or 'O'."
+        if self.instance:
+            if self.instance.status != GameStatus.RUNNING:
+                raise serializers.ValidationError("This game is already finished.")
+
+            diff_indexes = [
+                index
+                for index in range(len(board))
+                if self.instance.board[index] != board[index]
+            ]
+
+            if len(diff_indexes) == 0:
+                raise serializers.ValidationError("A new move must be made.")
+
+            if len(diff_indexes) >= 2:
+                raise serializers.ValidationError(
+                    "Only one move can be made at a time."
                 )
 
-        return value
+            if self.instance.board[diff_indexes[0]] != "-":
+                raise serializers.ValidationError(
+                    "A player can only make a move in an empty cell."
+                )
+
+            x_count = board.count("X")
+            o_count = board.count("O")
+            x_previous = self.instance.board.count("X")
+            o_previous = self.instance.board.count("O")
+
+            if x_count > x_previous and o_count > o_previous:
+                raise serializers.ValidationError(
+                    "Both X and O cannot make a move in the same turn."
+                )
+
+            if self.instance.player == GamePlayer.X and (
+                x_count == x_previous or o_count > o_previous
+            ):
+                raise serializers.ValidationError("X must make a move in this turn.")
+
+            if self.instance.player == GamePlayer.O and (
+                o_count == o_previous or x_count > x_previous
+            ):
+                raise serializers.ValidationError("O must make a move in this turn.")
+
+        else:
+            if (board.count("X") + board.count("O")) > 1:
+                raise serializers.ValidationError(
+                    "The board either must be empty or contain only one 'X' or 'O'."
+                )
+
+        return board
+
+    def create(self, validated_data):
+        """Create game, assign player and autoplay"""
+        instance = super().create(validated_data)
+
+        if not instance.player:
+            if "X" in instance.board:
+                instance.player = GamePlayer.X
+            elif "O" in instance.board:
+                instance.player = GamePlayer.O
+            else:
+                instance.player = random.choice(GamePlayer.values)
+
+        instance.autoplay()
+        instance.save()
+
+        return instance
+
+    def update(self, instance, validated_data):
+        """Update game, autoplay and update status"""
+        instance = super().update(instance, validated_data)
+
+        instance.autoplay()
+        instance.update_status()
+        instance.save()
+
+        return instance
 
     class Meta:
         model = Game
--- game/views.py
+++ game/views.py
@@ -12,10 +12,7 @@ from game.serializers import GameLocationSerializer, GameSerializer
         responses={
             200: OpenApiResponse(
                 response=GameSerializer,
-                description=(
-                    "Successful response, returns an array of games, "
-                    "returns an empty array if no users found"
-                ),
+                description="Returns an array of games or an empty array",
             ),
             400: OpenApiResponse(description="Bad request"),
             404: OpenApiResponse(description="Resource not found"),
@@ -38,15 +35,24 @@ from game.serializers import GameLocationSerializer, GameSerializer
         description="Get a game.",
         responses={
             200: OpenApiResponse(
-                response=GameSerializer,
-                description="Successful response, returns the game",
+                response=GameSerializer, description="Returns the game"
             ),
             400: OpenApiResponse(description="Bad request"),
             404: OpenApiResponse(description="Resource not found"),
             500: OpenApiResponse(description="Internal server error"),
         },
     ),
-    # update=extend_schema(description="Post a new move to a game."),
+    update=extend_schema(
+        description="Post a new move to a game.",
+        responses={
+            200: OpenApiResponse(
+                response=GameSerializer, description="Move successfully registered"
+            ),
+            400: OpenApiResponse(description="Bad request"),
+            404: OpenApiResponse(description="Resource not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    ),
     destroy=extend_schema(
         description="Delete a game.",
         responses={
@@ -65,7 +71,7 @@ class GameViewSet(viewsets.ModelViewSet):
     http_method_names = [
         "get",
         "post",
-        # "put",
+        "put",
         "delete",
         "options",
     ]
@@ -74,16 +80,11 @@ class GameViewSet(viewsets.ModelViewSet):
         """Return location link if game is successfully created"""
         response = super().create(request, *args, **kwargs)
 
-        # Check the game was created
         if response.status_code == status.HTTP_201_CREATED:
-            # Fetch created game object
             game = Game.objects.get(id=response.data.get("id"))
-            # Use a different serializer for game location
             serializer = GameLocationSerializer(game, context={"request": self.request})
-            # Get response headers
             headers = self.get_success_headers(serializer.data)
 
-            # Return response with game location and 201 status
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED,
