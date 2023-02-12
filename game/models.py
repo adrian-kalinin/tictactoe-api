c4d1d8d162891eaa4026a534e96843b2f3955bcc
Adrian Kalinin <adriankalinin@protonmail.com>
2023-02-12 21:18:01 +0200

    Add methods for autoplay and checking wins

--- game/models.py
+++ game/models.py
@@ -1,3 +1,4 @@
+import random
 import uuid
 
 from django.db import models
@@ -23,6 +24,17 @@ class GamePlayer(models.TextChoices):
 class Game(models.Model):
     """Database model for games"""
 
+    WIN_COMBINATIONS = [
+        [0, 1, 2],  # Upper row
+        [3, 4, 5],  # Middle row
+        [6, 7, 8],  # Bottom row
+        [0, 3, 6],  # Left column
+        [1, 4, 7],  # Middle column
+        [2, 5, 8],  # Right column
+        [0, 4, 8],  # Diagonal 1
+        [2, 4, 6],  # Diagonal 2
+    ]
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     board = models.CharField(max_length=9)
     status = models.CharField(
@@ -31,6 +43,27 @@ class Game(models.Model):
     player = models.CharField(
         max_length=1, choices=GamePlayer.choices, default=None, null=True
     )
+
+    def autoplay(self):
+        """Make a new move on the board"""
+        empty_cells = [index for index, cell in enumerate(self.board) if cell == "-"]
+        next_move = random.choice(empty_cells)
+
+        if next_move:
+            board = list(self.board)
+            board[next_move] = "X" if self.player == GamePlayer.O else "O"
+            self.board = "".join(board)
+
+    def update_status(self):
+        """Update game status based on the current board"""
+        if "-" not in self.board:
+            self.status = GameStatus.DRAW
+
+        for combination in self.WIN_COMBINATIONS:
+            if all([self.board[index] == "X" for index in combination]):
+                self.status = GameStatus.X_WON
+            elif all([self.board[index] == "O" for index in combination]):
+                self.status = GameStatus.O_WON
 
     def __str__(self):
         """String representation of a game"""
